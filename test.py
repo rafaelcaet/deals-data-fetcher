94e729a64279c17f593ddd7b057421b4acd9743b
Rafael Caetano <fael.caetsantos@gmail.com>
2024-07-01 17:43:46 -0300

    fix: dynamic creating dataframe

--- test.py
+++ test.py
@@ -2,38 +2,41 @@ import requests
 import pandas as pd 
 import json
 import sys
-
+import time
 
 class RequestApi():
     def __init__(self):
         pass
     
-    def getAPIResponse(self,endpoint,endpointName):
-        response =  requests.get(endpoint["api_url"], headers=endpoint["headers"])
-        # print(response.json())
-        dataFrame = pd.DataFrame(response.json())
-        dataFrame.to_csv(endpointName+".csv", encoding='utf-8', index=False)
-        print(dataFrame)  
-           
+    def getAPIResponse(self, endpoint, endpointName):
+        response = requests.get(endpoint["api_url"], headers=endpoint["headers"])
+        data = response.json()
+        if endpoint["schema"].keys() is not None:
+            dataFrameData = pd.DataFrame(data,columns=endpoint["schema"].keys())
+        else:
+            dataFrameData = pd.DataFrame(data)
+        dataFrameData.to_csv(endpointName + ".csv", encoding='utf-8', index=False)
 
 def parseArgs():
     args = sys.argv[1:]
     return args
 
 def main():
-    
-    #convert interface file to JSON
+    # Convert interface file to JSON
     with open('interface.json', 'r') as f:
         interface = json.load(f) 
 
     test = RequestApi()
     
-    endpoint = parseArgs().pop()
-    print("######## creating <"+endpoint+"> dataframe...################")
-    test.getAPIResponse(interface[endpoint],endpoint)
-    print("############################################################")
-
-
+    endpoint = parseArgs()
+    for ep in endpoint:
+        print("############################################################")
+        print("creating <"+ep+"> dataframe...")
+        time.sleep(1)
+        test.getAPIResponse(interface[ep], ep)
+        time.sleep(1)
+        print("<"+ep+"> dataframe finalized")
+    print("###########################################################")
 
 if __name__ == "__main__":
     main()
